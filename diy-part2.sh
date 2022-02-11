27253b5e938e9ad8c5e04a78302ec27645064b7c
Minimon <aolinver@gmail.com>
2022-02-11 16:46:26 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,18 +11,10 @@
 #
 
 # Add themes
-rm -rf package/lean/luci-theme-argon
-git clone https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
-
 git clone https://github.com/XXKDB/luci-theme-argon_armygreen.git package/luci-theme-argon_armygreen
-
 git clone https://github.com/thinktip/luci-theme-neobird.git package/luci-theme-neobird
-
-#git clone https://github.com/toss-a/luci-theme-atmaterial.git package/luci-theme-atmaterial
-#sed -i 's/luci.themes.ATMaterial/luci.themes.ATMaterial_DockerIcon/g' package/luci-theme-atmaterial/files/30_luci-theme-atmaterial
-
+git clone https://github.com/toss-a/luci-theme-atmaterial.git package/luci-theme-atmaterial
 git clone https://github.com/virualv/luci-theme-pink.git package/luci-theme-pink
-
 git clone https://github.com/thomaswcy/luci-theme-rubik.git package/luci-theme-rubik
 
 # Modify default IP
